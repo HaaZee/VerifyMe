a62077cd05802d9a0d4c5641e9d946ccba179903
HaaZee <33757920+HaaZee@users.noreply.github.com>
2019-01-10 17:02:50 +0000

    Update cmd_verify.py

--- commands/cmd_verify.py
+++ commands/cmd_verify.py
@@ -62,6 +62,7 @@ async def ex(args, message, client, invoke):
 
                             await client.send_message(message.author, embed=discord.Embed(color=discord.Color.green(), description="You are now verified. Have fun playing!"))
                             await client.add_roles(author, verified)
+                            await client.remove_roles(author, unverified)
 
                         else:
                             await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="Sorry, your verifcation request was not approved. You will not be able to play RevengeEU scrims."))
