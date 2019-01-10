6ccc83d609951559a2330d2912d49ce49ec84c16
HaaZee <33757920+HaaZee@users.noreply.github.com>
2019-01-10 17:47:16 +0000

    Update cmd_verify.py

--- commands/cmd_verify.py
+++ commands/cmd_verify.py
@@ -7,43 +7,42 @@ import time
 from discord.utils import get
 
 async def ex(args, message, client, invoke):
+    verified_role = "Verified"
+    if verified_role.lower() not in [y.name.lower() for y in author.roles]:
 
-    report_embed = discord.Embed(title="VerifyMe:" , color=discord.Color.green())
-    report_embed.add_field(name="What to do: ", value="To verify your account so you can participate in RevengeEU scrims please respond with your STEAM64 ID.")
-    report_embed.add_field(name="How?: ", value="*You can get this using https://steamidfinder.com/.*")
-    report_embed.set_footer(text="NOTE: All profile settings must be set to public.")
+        report_embed = discord.Embed(title="VerifyMe:" , color=discord.Color.green())
+        report_embed.add_field(name="What to do: ", value="To verify your account so you can participate in RevengeEU scrims please respond with your STEAM64 ID.")
+        report_embed.add_field(name="How?: ", value="*You can get this using https://steamidfinder.com/.*")
+        report_embed.set_footer(text="NOTE: All profile settings must be set to public.")
 
-    await client.send_message(message.author, embed=report_embed)
+        await client.send_message(message.author, embed=report_embed)
 
-    author = message.author
+        author = message.author
 
-    message = await client.wait_for_message(author=author)
+        message = await client.wait_for_message(author=author)
 
-    if message.server is None and message.author != client.user:
+        if message.server is None and message.author != client.user:
 
-        steamid = message.content
+            steamid = message.content
 
-        if steamid.isdigit() and len(steamid) == 17:
+            if steamid.isdigit() and len(steamid) == 17:
 
-            await client.send_message(message.author, embed=discord.Embed(color=discord.Color.orange(), description="Your verification request has gone through, you will be verified shortly."))
+                await client.send_message(message.author, embed=discord.Embed(color=discord.Color.orange(), description="Your verification request has gone through, you will be verified shortly."))
 
-            url =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=721E1DBC94F54A914E9B0AE1250B1C5D&steamid={}&format=json".format(steamid)
-            username_url =  "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=721E1DBC94F54A914E9B0AE1250B1C5D&steamids={}&format=json".format(steamid)
+                url =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=721E1DBC94F54A914E9B0AE1250B1C5D&steamid={}&format=json".format(steamid)
+                username_url =  "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=721E1DBC94F54A914E9B0AE1250B1C5D&steamids={}&format=json".format(steamid)
 
-            r = requests.get(url)
+                r = requests.get(url)
 
-            for game in r.json()['response']['games']:
-                if game['appid'] == 433850:
-                    minutes = game['playtime_forever']
+                for game in r.json()['response']['games']:
+                    if game['appid'] == 433850:
+                        minutes = game['playtime_forever']
 
-            game_time = minutes / 60
-            game_time = round(game_time, 1)
+                game_time = minutes / 60
+                game_time = round(game_time, 1)
 
-            verified = get(author.server.roles, name="Verified")
-            verified_role = "Verified"
-            verif_code = random.randint(1000000, 9000000)
-
-            if verified_role.lower() not in [y.name.lower() for y in author.roles]:
+                verified = get(author.server.roles, name="Verified")
+                verif_code = random.randint(1000000, 9000000)
 
                 if game_time > 50:
                     await client.send_message(message.author, embed=discord.Embed(color=discord.Color.orange(), description="To confirm the authenticity of this account, please add this verification code to your steam name: **{}**\n*Please reply to this message with 'done' once you have completed the task.*".format(verif_code)))
@@ -73,9 +72,8 @@ async def ex(args, message, client, invoke):
                 else:
                     await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="Sorry, your verifcation request was not approved. You will not be able to play RevengeEU scrims."))
 
-
             else:
-                await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="You are already verified."))
+                await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="Must be STEAM64 ID.\n *Verification closed, please retype the command in RevengeEU*"))
 
-        else:
-            await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="Must be STEAM64 ID.\n *Verification closed, please retype the command in RevengeEU*"))
+    else:
+        await client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description="You are already verified."))
