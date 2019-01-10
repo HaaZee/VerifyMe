005a73b885f84fee613fb64f157162306ef9bcf4
HaaZee <33757920+HaaZee@users.noreply.github.com>
2019-01-10 16:01:51 +0000

    Update cmd_verify.py

--- commands/cmd_verify.py
+++ commands/cmd_verify.py
@@ -32,51 +32,45 @@ async def ex(args, message, client, invoke):
             r = requests.get(url)
             r_user = requests.get(url)
 
-            try:
-                for game in r.json()['response']['games']:
-                    if game['appid'] == 433850:
-                        minutes = game['playtime_forever']
+            for game in r.json()['response']['games']:
+                if game['appid'] == 433850:
+                    minutes = game['playtime_forever']
 
-                game_time = minutes / 60
-                game_time = round(game_time, 1)
+            game_time = minutes / 60
+            game_time = round(game_time, 1)
 
-                verified = get(author.server.roles, name="Verified")
-                unverified = get(author.server.roles, name="Unverified")
-                verified_role = "Verified"
-                verif_code = random.randint(1000000, 9000000)
+            verified = get(author.server.roles, name="Verified")
+            unverified = get(author.server.roles, name="Unverified")
+            verified_role = "Verified"
+            verif_code = random.randint(1000000, 9000000)
 
-                if verified_role.lower() not in [y.name.lower() for y in author.roles]:
+            if verified_role.lower() not in [y.name.lower() for y in author.roles]:
 
-                    if game_time > 10:
-                        await client.send_message(message.author, embed=discord.Embed(color=discord.Color.green(), description="To confirm the authenticity of this account, please add this verification code to your steam name: **{}**\n*Please reply to this message with 'done' once you have completed the task.*".format(verif_code)))
-                        message = await client.wait_for_message(author=author)
-                        if message.content.lower() == "done":
-
-
-                            for item in r_user.json()['response']['players']:
-                                for value in item:
-                                    if value == "personaname":
-                                        name = item[value]
-
-                            if verif_code in name:
-
-                                await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.green(), description="You are now verified. Have fun playing!"))
-                                await client.add_roles(author, verified)
-                        else:
-                            await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Must reply with 'done'.\n *Verification closed, please retype the command in RevengeEU*"))
+                if game_time > 10:
+                    await client.send_message(message.author, embed=discord.Embed(color=discord.Color.green(), description="To confirm the authenticity of this account, please add this verification code to your steam name: **{}**\n*Please reply to this message with 'done' once you have completed the task.*".format(verif_code)))
+                    message = await client.wait_for_message(author=author)
+                    if message.content.lower() == "done":
 
 
+                        for item in r_user.json()['response']['players']:
+                            for value in item:
+                                if value == "personaname":
+                                    name = item[value]
+
+                        if verif_code in name:
+
+                            await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.green(), description="You are now verified. Have fun playing!"))
+                            await client.add_roles(author, verified)
                     else:
-                        await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Sorry, your verifcation request was not approved. You will not be able to play RevengeEU scrims."))
-                        await client.add_roles(author, unverified)
+                        await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Must reply with 'done'.\n *Verification closed, please retype the command in RevengeEU*"))
+
 
                 else:
-                    await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="You are already verified."))
+                    await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Sorry, your verifcation request was not approved. You will not be able to play RevengeEU scrims."))
+                    await client.add_roles(author, unverified)
 
-
-
-            except KeyError:
-                await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Profile must be set to public so game hours can be viewed."))
+            else:
+                await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="You are already verified."))
 
         else:
             await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description="Must be STEAM64 ID.\n *Verification closed, please retype the command in RevengeEU*"))
