fee87f90e23d63f7bcb7fb9d923f7863fd798b87
HaaZee <33757920+HaaZee@users.noreply.github.com>
2019-01-17 17:59:33 +0000

    Update cmd_help.py

--- commands/cmd_help.py
+++ commands/cmd_help.py
@@ -3,9 +3,10 @@ import discord
 async def ex(args, message, client, invoke):
 
     verify = "**.verify** - Allows you to verify your account so you can participate in scrims."
+    botinfo = "**.botinfo** - Prints info about VerifyMe."
 
     embed = discord.Embed(title="COMMANDS:", description=" ", color=0x00ff00)
-    embed.add_field(name="__**Default Commands**__", value="\n{}".format(verify), inline=True)
+    embed.add_field(name="__**Default Commands**__", value="\n{}\n{}".format(verify, botinfo), inline=True)
     embed.set_footer(text="Bot created by: HaZe#7197 ")
 
     await client.send_message(message.author, embed=embed)
